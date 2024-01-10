855d491352fadad5d1f3e54d6e701480bde175f1
João Pedro Volponi <jpvrosada@hotmail.com>
2024-01-09 22:14:56 -0300

    feat: 🎸 Adicionando Label BF NFT

--- Login/LoginScreen.swift
+++ Login/LoginScreen.swift
@@ -23,10 +23,21 @@ class LoginScreen: UIView {
         return image
     }()
     
+    lazy var loginLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.text = "BF NFT"
+        return label
+    }()
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.addSubview(self.subImageView)
         self.addSubview(self.logoAppImageView)
+        self.addSubview(self.loginLabel)
         self.configConstraints()
     }
     
@@ -44,7 +55,10 @@ class LoginScreen: UIView {
             self.logoAppImageView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor,constant: 55),
             self.logoAppImageView.heightAnchor.constraint(equalToConstant: 108),
             self.logoAppImageView.widthAnchor.constraint(equalToConstant: 108),
-            self.logoAppImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+            self.logoAppImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            
+            self.loginLabel.topAnchor.constraint(equalTo: self.logoAppImageView.bottomAnchor, constant: 16),
+            self.loginLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor)
         ])
     }
 }
